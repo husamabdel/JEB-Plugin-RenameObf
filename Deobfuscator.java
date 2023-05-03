1f0257383b1dbd07425eff3871ecab770dc5148e
husamabdel <husamabdalla98@gmail.com>
2023-05-03 00:09:13 -0400

    DeObfuscator
    DeObfuscator

--- Deobfuscator.java
+++ Deobfuscator.java
@@ -2,7 +2,11 @@ import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.pnfsoftware.jeb.core.IEnginesContext;
+import com.pnfsoftware.jeb.core.units.IBinaryUnit;
 import com.pnfsoftware.jeb.core.units.code.IClass;
+import com.pnfsoftware.jeb.core.units.code.IDecompilerUnit;
+import com.pnfsoftware.jeb.core.units.code.IDecompiledUnit;
 
 public class Deobfuscator {
     
@@ -10,6 +14,20 @@ public class Deobfuscator {
     private static final Pattern CLASS_NAME_PATTERN = Pattern.compile("(\\w+\\.)*\\w+");
 
     public static void main(String[] args) {
+        
+        // Obtain the JEB engines context and binary unit that contains the class to deobfuscate
+        IEnginesContext context = ...; // obtain the JEB engines context
+        IBinaryUnit binaryUnit = ...; // obtain the binary unit containing the class
+        
+        // Obtain the decompiler unit for the binary unit
+        IDecompilerUnit decompilerUnit = context.getUnitFactory().getDecompiler(binaryUnit);
+        
+        // Obtain the decompiled unit for the class
+        IClass decompiledClass = ...; // obtain the decompiled class using the JEB API
+        
+        // Obtain the decompiled code for the class
+        IDecompiledUnit decompiledUnit = decompilerUnit.decompileClass(decompiledClass);
+        String decompiledCode = decompiledUnit.getSource();
         
         // Create a mapping to store the obfuscated class names and their corresponding identified class names
         HashMap<String, String> classMapping = new HashMap<String, String>();
@@ -36,8 +54,7 @@ public class Deobfuscator {
         }
         
         // Use the JEB API to update the decompiled code with the renamed class names
-        IClass decompiledClass = // obtain the decompiled class using the JEB API
-        decompiledClass.updateContent(decompiledCode);
+        decompiledUnit.setContent(decompiledCode);
     }
     
     // This method should search the decompiled code for the obfuscated class name and return the identified class name
